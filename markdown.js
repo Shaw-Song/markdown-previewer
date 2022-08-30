23671ff0281d8874bf5762133c1cd70d345ae72e
shaw <shawsong2022@gmail.com>
2022-08-30 00:48:37 -0700

    pic change

--- markdown.js
+++ markdown.js
@@ -20,7 +20,7 @@ Or _italic_.
 Or... wait for it... **_both!_**
 And feel free to go crazy ~~crossing stuff out~~.
 
-There's also [links](https://shaw-song.github.io/), and
+There's also [links](https://shaw-song.github.io/yummy-farm/), and
 > Block Quotes!
 
 And if you want to get really crazy, even tables:
@@ -40,7 +40,7 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+![Yummy Farm Logo](https://shaw-song.github.io/yummy-farm/logo.jpg)
 `;
 
 marked.use({
